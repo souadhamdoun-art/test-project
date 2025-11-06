9db1ee1d9c15a43c49e043d1f417507852b2eea8
souad.hamdoun <souad.hamdoun@maisonduweb.com>
2025-11-06 11:12:17 +0100

    fix: Register Reviews module routes
    - Remove redundant ->names() from Reviews routes
    - Register Reviews module routes in AppServiceProvider
    - Fix route registration for reviews.store functionality

--- Modules/Reviews/routes/web.php
+++ Modules/Reviews/routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\Reviews\Http\Controllers\ReviewsController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('reviews', ReviewsController::class)->names('reviews');
+    Route::resource('reviews', ReviewsController::class);
 });
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Enregistrer les routes du module Reviews
+        $this->loadRoutesFrom(base_path('Modules/Reviews/routes/web.php'));
     }
 }
